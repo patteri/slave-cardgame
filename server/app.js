4b6e30adb2299860d2f1e217e5ac812ae35c0b29
patteri <petteri.roponen@gmail.com>
2017-04-24 13:27:09 +0300

    Fixes for production configuration

--- server/app.js
+++ server/app.js
@@ -2,20 +2,28 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const path = require('path');
 
 // Create and configure Express app
 const app = express();
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(logger('dev'));
+}
 
 // Register routes
 app.use(routes);
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  const pathName = path.join(__dirname, '../client/build');
+  app.use(express.static(pathName));
+  app.get('*', (req, res) => {
+    res.sendFile(pathName + '/index.html');
+  });
 }
 
 module.exports = app;
